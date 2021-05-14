137271343d901bde3327f51b1997da0712185caa
Laurens Van Houtven (lvh) <_@lvh.io>
2021-05-13 19:25:50 -0500

    Oops

--- variables.tf
+++ variables.tf
@@ -189,7 +189,7 @@ locals {
   zone_name                = data.aws_route53_zone.zone.name
   non_verification_records = var.use_dnssec_signed_records ? var.dnssec_mx_records : var.mx_records
   verification_record      = var.mx_verification_record_prefix != null ? format("%g %s%s", var.mx_verification_record_priority, var.mx_verification_record_prefix, var.mx_verification_record_suffix) : null
-  gmail_records            = local.verification_record != null ? concat(non_verification_records, [local.verification_record]) : non_verification_records
+  gmail_records            = local.verification_record != null ? concat(local.non_verification_records, [local.verification_record]) : local.non_verification_records
   dmarc_report_recipient   = var.dmarc_report_recipient != null ? var.dmarc_report_recipient : format("hostmaster@%s", local.zone_name)
   dmarc_policy             = "v=${var.dmarc_protocol_version}; p=${var.dmarc_policy_type}; pct=${var.dmarc_policy_percentage}; rua=mailto:${local.dmarc_report_recipient}; sp=${var.dmarc_subdomain_policy_type} adkim=${var.dmarc_dkim_alignment_mode}; aspf=${var.dmarc_spf_alignment_mode}"
   apex_txt_record          = var.apex_verification_txt != null ? [var.apex_spf_txt, var.apex_verification_txt] : [var.apex_spf_txt]
